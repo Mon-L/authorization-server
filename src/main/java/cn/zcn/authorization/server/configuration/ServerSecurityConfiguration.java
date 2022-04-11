4ca824f24246dda588a507ba888fa87f46cf850c
chenzhicong <chenzhicong01@agree.com.cn>
2022-04-11 16:07:11 +0800

    配置 OAuth2 端点异常处理

--- src/main/java/cn/zcn/authorization/server/configuration/ServerSecurityConfiguration.java
+++ src/main/java/cn/zcn/authorization/server/configuration/ServerSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package cn.zcn.authorization.server.configuration;
 
 import cn.zcn.authorization.server.configurer.ServerSecurityConfigurer;
+import cn.zcn.authorization.server.exception.OAuth2ExceptionWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import java.util.List;
 
@@ -33,13 +35,20 @@ public class ServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         serverSecurityConfigurer.init(http);
         RequestMatcher requestMatcher = serverSecurityConfigurer.getRequestMatcher();
+        OAuth2ExceptionWriter oAuth2ExceptionWriter = http.getSharedObject(OAuth2ExceptionWriter.class);
 
         http
                 .requestMatcher(requestMatcher).authorizeRequests().anyRequest().fullyAuthenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
                 .and()
-                .csrf().ignoringRequestMatchers(requestMatcher);
+                .csrf().ignoringRequestMatchers(requestMatcher)
+                .and()
+                .exceptionHandling()
+                .defaultAuthenticationEntryPointFor((request, response, exception) -> {
+                    oAuth2ExceptionWriter.write(exception, new ServletWebRequest(request, response));
+                    response.flushBuffer();
+                }, requestMatcher);
 
         for (SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> configurer : serverSecurityConfigurer.getConfigurers().values()) {
             http.apply(configurer);
